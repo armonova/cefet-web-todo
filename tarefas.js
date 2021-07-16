97251cd3701cdd96b629a9422557af0a71b161f1
Arthur Novaes <arthurnovaes0@gmail.com>
2021-07-15 21:53:52 -0300

    ex1

--- tarefas.js
+++ tarefas.js
@@ -1,10 +1,33 @@
-
-// Exercício 0
+class Categoria {
+    static LAZER = 'lazer';
+    static COMPRAS = 'compras';
+    static ESTUDOS = 'estudos';
+    static categoria(categoria) {
+        return `categoria-${categoria}`
+    }
+}
 class Tarefa {
     constructor(nome, categoria, realizada) {
         this.nome = nome;
         this.categoria = categoria;
         this.realizada = realizada;
+    }
+    adicionaNaPagina(containerEl) {
+        const li = document.createElement('li');
+        
+        li.innerHTML = this.nome;
+        li.classList.add(
+            'item-tarefa',
+            this.realizada ? 'marcado' : null,
+            Categoria.categoria(this.categoria)
+        )
+
+        containerEl.appendChild(li);
+
+        li.addEventListener('click', () => {
+            li.classList.toggle('marcado');
+            this.realizada = !this.realizada;
+        });
     }
 }
 
@@ -13,3 +36,10 @@ let tarefa2 = new Tarefa('Escutar chimbinha', 'lazer', true);
 
 
 let tarefas = [tarefa1, tarefa2];
+
+listaTarefas = document.querySelector('#lista-tarefas');
+listaTarefas.innerHTML = '';
+
+tarefas.forEach(tarefa => {
+    tarefa.adicionaNaPagina(listaTarefas);
+})
